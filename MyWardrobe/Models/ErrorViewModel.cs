02244bcef0c93becda9c308a84bfb00ed1c86da9
JNing73 <94809226+JNing73@users.noreply.github.com>
2025-02-07 15:54:41 +1300

    Initialise a new asp.net core mvc project

---
+++ MyWardrobe/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MyWardrobe.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
